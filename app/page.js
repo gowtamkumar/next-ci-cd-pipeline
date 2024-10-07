2fcd0b09fde85d426a997f8a3bcab8777601ac7b
gowtamkumar <gowtampaul0@gmail.com>
2024-10-07 12:47:52 +0600

    ddd

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">Get started by learn ci/cd pipline</li>
-          <li>Save and see your changes instantly.</li>
+          <li>Learn ci/cd pipline for begenner</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
