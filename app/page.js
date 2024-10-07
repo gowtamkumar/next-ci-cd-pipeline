b66617ba83547c7ca9ccf0f624ca0def7b961c22
gowtamkumar <gowtampaul0@gmail.com>
2024-10-07 12:36:59 +0600

    ci/cd

--- app/page.js
+++ app/page.js
@@ -13,13 +13,7 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
+          <li className="mb-2">Get started by learn ci/cd pipline</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
